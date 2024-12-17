5c42b747de2d6dec353313149495a31435f6c3fa
universechoi <skye.of.universe@gmail.com>
2024-12-17 18:00:33 +0900

    feat: add findBySubjectLike() in QuestionRepository

--- src/main/java/com/ll/sbb/question/QuestionRepository.java
+++ src/main/java/com/ll/sbb/question/QuestionRepository.java
@@ -2,8 +2,12 @@ package com.ll.sbb.question;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Question findBySubject(String subject);
 
     Question findBySubjectAndContent(String Subject, String Content);
+
+    List<Question> findBySubjectLike(String Subject);
 }
