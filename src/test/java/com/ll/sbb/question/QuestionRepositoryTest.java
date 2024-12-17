06c69b5508ce655444a7d2c18f5374f8ca36f23d
universechoi <skye.of.universe@gmail.com>
2024-12-17 18:17:47 +0900

    refactor(test): reorganize test folder structure

---
+++ src/test/java/com/ll/sbb/question/QuestionRepositoryTest.java
@@ -0,0 +1,101 @@
+package com.ll.sbb.question;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class QuestionRepositoryTest {
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @Test
+    @DisplayName("questionRepository.save()")
+    void saveTest() {
+        Question q1 = new Question();
+        q1.setSubject("sbb가 무엇인가요?");
+        q1.setContent("sbb에 대해 알고 싶습니다.");
+        this.questionRepository.save(q1);
+
+        Question q2 = new Question();
+        q2.setSubject("스프링 부트 모델 질문입니다.");
+        q2.setContent("id는 자동으로 생성되나요?");
+        this.questionRepository.save(q2);
+    }
+
+    @Test
+    @DisplayName("findAll()")
+    void findAllTest() {
+        List<Question> all = this.questionRepository.findAll();
+        assertEquals(2, all.size());
+
+        Question q = all.get(0);
+        assertEquals("sbb가 무엇인가요?", q.getSubject());
+    }
+
+    @Test
+    @DisplayName("findById()")
+    void findByIdTest() {
+        Optional<Question> oq = this.questionRepository.findById(1);
+        if (oq.isPresent()) {
+            Question q = oq.get();
+            assertEquals("sbb가 무엇인가요?", q.getSubject());
+        }
+    }
+
+    @Test
+    @DisplayName("findBySubject()")
+    void findBySubjectTest() {
+        Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
+        assertEquals(1, q.getId());
+    }
+
+    @Test
+    @DisplayName("findBySubjectAndContent()")
+    void findBySubjectAndContentTest() {
+        Question q = this.questionRepository.findBySubjectAndContent("sbb가 무엇인가요?", "sbb에 대해 알고 싶습니다.");
+        assertEquals(1, q.getId());
+    }
+
+    @Test
+    @DisplayName("findBySubjectLike()")
+    void findBySubjectLikeTest() {
+        List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
+        Question q = qList.get(0);
+        assertEquals("sbb가 무엇인가요?", q.getSubject());
+    }
+
+    @Test
+    @DisplayName("setSubject()")
+    @Transactional
+    void modifyTest() {
+        Optional<Question> oq = this.questionRepository.findById(1);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
+        q.setSubject("수정된 제목");
+        this.questionRepository.save(q);
+    }
+
+    @Test
+    @DisplayName("delete()")
+    @Transactional
+    void deleteTest() {
+        assertEquals(2, this.questionRepository.count());
+        Optional<Question> oq = this.questionRepository.findById(1);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
+        this.questionRepository.delete(q);
+        assertEquals(1, this.questionRepository.count());
+    }
+}
