76bf752c8f4316033822fad60f3d3536b3eb2b78
universechoi <skye.of.universe@gmail.com>
2024-12-17 17:45:30 +0900

    feat: add findBySubject() in QuestionRepository

--- src/main/java/com/ll/sbb/question/QuestionRepository.java
+++ src/main/java/com/ll/sbb/question/QuestionRepository.java
@@ -3,4 +3,5 @@ package com.ll.sbb.question;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
+    Question findBySubject(String subject);
 }
