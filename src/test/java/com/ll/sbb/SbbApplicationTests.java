d885a37f2d97f804f3be11aeef2ce32e653947e2
universechoi <skye.of.universe@gmail.com>
2024-12-17 17:41:11 +0900

    test: save()

--- src/test/java/com/ll/sbb/SbbApplicationTests.java
+++ src/test/java/com/ll/sbb/SbbApplicationTests.java
@@ -1,15 +1,33 @@
 package com.ll.sbb;
 
+import com.ll.sbb.question.Question;
+import com.ll.sbb.question.QuestionRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class SbbApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Autowired
+    private QuestionRepository questionRepository;
 
+    @Test
+    @DisplayName("questionRepository.save()")
+    @Transactional
+    void saveTest() {
+        Question q1 = new Question();
+        q1.setSubject("sbb가 무엇인가요?");
+        q1.setContent("sbb에 대해 알고 싶습니다.");
+        this.questionRepository.save(q1);
+
+        Question q2 = new Question();
+        q2.setSubject("스프링 부트 모델 질문입니다.");
+        q2.setContent("id는 자동으로 생성되나요?");
+        this.questionRepository.save(q2);
+    }
 }
