20f0a7f819a26f118df67a7087ece8ba219bff59
universechoi <skye.of.universe@gmail.com>
2024-12-17 17:57:06 +0900

    test: findBySubjectAndContent()

--- src/test/java/com/ll/sbb/SbbApplicationTests.java
+++ src/test/java/com/ll/sbb/SbbApplicationTests.java
@@ -60,4 +60,11 @@ class SbbApplicationTests {
         Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
         assertEquals(1, q.getId());
     }
+
+    @Test
+    @DisplayName("findBySubjectAndContent()")
+    void findBySubjectAndContentTest() {
+        Question q = this.questionRepository.findBySubjectAndContent("sbb가 무엇인가요?", "sbb에 대해 알고 싶습니다.");
+        assertEquals(1, q.getId());
+    }
 }
