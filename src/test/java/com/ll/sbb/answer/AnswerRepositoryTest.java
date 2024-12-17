bc388335263389144bf21d56b9c2c6c837cec000
universechoi <skye.of.universe@gmail.com>
2024-12-17 18:25:40 +0900

    test: save()

---
+++ src/test/java/com/ll/sbb/answer/AnswerRepositoryTest.java
@@ -0,0 +1,37 @@
+package com.ll.sbb.answer;
+
+import com.ll.sbb.question.Question;
+import com.ll.sbb.question.QuestionRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class AnswerRepositoryTest {
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @Autowired
+    private AnswerRepository answerRepository;
+
+    @Test
+    @DisplayName("save()")
+    void saveTest() {
+        Optional<Question> oq = this.questionRepository.findById(2);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
+
+        Answer a = new Answer();
+        a.setContent("네 자동으로 생성됩니다.");
+        a.setQuestion(q);
+        this.answerRepository.save(a);
+    }
+}
