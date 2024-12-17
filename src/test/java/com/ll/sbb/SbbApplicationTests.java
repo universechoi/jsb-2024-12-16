8e033acd430fa22e6bcf48a5460ceb5b9b5a4483
universechoi <skye.of.universe@gmail.com>
2024-12-17 17:11:36 +0900

    test: separate profile to test

--- src/test/java/com/ll/sbb/SbbApplicationTests.java
+++ src/test/java/com/ll/sbb/SbbApplicationTests.java
@@ -2,8 +2,10 @@ package com.ll.sbb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SbbApplicationTests {
 
     @Test
