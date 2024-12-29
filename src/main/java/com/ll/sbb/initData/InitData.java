df4450e80f93bbfbe5d5b76fabef23eb675d1a94
universechoi <skye.of.universe@gmail.com>
2024-12-29 16:04:16 +0900

    feat: add siteUser to initData

--- src/main/java/com/ll/sbb/initData/InitData.java
+++ src/main/java/com/ll/sbb/initData/InitData.java
@@ -1,6 +1,7 @@
 package com.ll.sbb.initData;
 
 import com.ll.sbb.question.QuestionService;
+import com.ll.sbb.user.SiteUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +15,14 @@ public class InitData {
 
     @Bean
     public ApplicationRunner initDataApplicationRunner() {
+
+        SiteUser siteUser = SiteUser
+                .builder()
+                .username("newUser")
+                .password("1234567890")
+                .email("user@gmail.com")
+                .build();
+
         return args -> {
             if (questionService.count() > 0) {
                 return ;
@@ -21,7 +30,7 @@ public class InitData {
             for (int i = 0; i <= 300; i++) {
                 String subject = String.format("테스트 데이터입니다.:[%03d]", i);
                 String content = "내용 없음";
-                this.questionService.create(subject, content);
+                this.questionService.create(subject, content, siteUser);
             }
         };
     }
