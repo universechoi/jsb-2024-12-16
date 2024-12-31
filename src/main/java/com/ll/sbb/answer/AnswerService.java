573c8f61f29942b0fc7a1b7c6acf21df6d0d7822
universechoi <skye.of.universe@gmail.com>
2024-12-31 15:31:59 +0900

    feat: modify answer

--- src/main/java/com/ll/sbb/answer/AnswerService.java
+++ src/main/java/com/ll/sbb/answer/AnswerService.java
@@ -1,9 +1,12 @@
 package com.ll.sbb.answer;
 
+import com.ll.sbb.DataNotFoundException;
 import com.ll.sbb.question.Question;
 import com.ll.sbb.user.SiteUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -14,6 +17,21 @@ public class AnswerService {
     public void create(Question question, String content, SiteUser author) {
         Answer answer = Answer.builder().question(question).content(content).author(author).build();
 
+        this.answerRepository.save(answer);
+    }
+
+    public Answer getAnswer(Integer id) {
+        Optional<Answer> answer = this.answerRepository.findById(id);
+
+        if (answer.isPresent()) {
+            return answer.get();
+        } else {
+            throw new DataNotFoundException(("answer not found"));
+        }
+    }
+
+    public void modify(Answer answer, String content) {
+        answer.setContent(content);
         this.answerRepository.save(answer);
     }
 }
